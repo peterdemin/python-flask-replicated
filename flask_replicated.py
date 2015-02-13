284ef9d63a6083df9ec8243719d3728321fbafb9
Peter Demin <peterdemin@yandex-team.ru>
2015-02-13 13:36:38 +0300

    initial commit

---
+++ flask_replicated.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+
+from flask import current_app, g, request
+
+
+class FlaskReplicated(object):
+    READONLY_METHODS = set(['GET', 'HEAD'])
+
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        assert hasattr(app, 'extensions')
+        assert 'sqlalchemy' in app.extensions
+        if 'replicated' not in app.extensions:
+            app.extensions['replicated'] = self
+            binds = app.config.get('SQLALCHEMY_BINDS') or {}
+            if 'slave' in binds:
+                app.before_request(self._pick_database_replica)
+                db = app.extensions['sqlalchemy'].db
+                get_engine_vanilla = db.get_engine
+
+                def get_replicated_engine(app, bind=None):
+                    if bind is None:
+                        use_slave = getattr(g, 'use_slave', False)
+                        use_master = getattr(g, 'use_master', False)
+                        if use_slave and not use_master:
+                            bind = 'slave'
+                    return get_engine_vanilla(app, bind)
+
+                db.get_engine = get_replicated_engine
+
+    def _pick_database_replica(self):
+        func = current_app.view_functions.get(request.endpoint)
+        if getattr(func, 'use_master_database', False):
+            g.use_master = True
+        g.use_slave = request.method in self.READONLY_METHODS
+
+
+def changes_database(func):
+    func.use_master_database = True
+    return func
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+
+setup(
+    name='flask_replicated',
+    version='1.0',
+    py_modules=['flask_replicated'],
+)
